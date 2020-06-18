8d2df4651676b3714f3808e3df69c5052500f4ae
Hugo Ledoux <h.ledoux@tudelft.nl>
2020-06-18 14:58:03 +0200

    Reorganise the code

--- src/sstfin/bin/main.rs
+++ src/sstfin/bin/main.rs
@@ -26,6 +26,12 @@ pub struct Point {
     pub z: f64,
 }
 
+#[derive(PartialEq, Eq)]
+enum InputType {
+    LAS,
+    XYZ,
+}
+
 impl Point {
     pub fn new() -> Point {
         Point {
@@ -54,7 +60,7 @@ fn main() {
 
     env_logger::init();
 
-    let mut inputmode: u8 = 0; //0-LAS; 1-XYZ
+    let mut inputformat: InputType = InputType::LAS;
 
     let mut paths: Vec<String> = Vec::new();
     let t1 = matches.value_of("INPUT").unwrap();
@@ -66,7 +72,7 @@ fn main() {
     {
         paths.push(path.to_str().unwrap().to_string());
     } else if path.extension().unwrap() == "xyz" {
-        inputmode = 1;
+        inputformat = InputType::XYZ;
         paths.push(path.to_str().unwrap().to_string());
     } else if path.extension().unwrap() == "files" {
         println!("input files");
@@ -82,60 +88,205 @@ fn main() {
 
     let cellsize: usize = matches.value_of("RESOLUTION").unwrap().parse().unwrap();
 
-    if inputmode == 0 {
-        //-- pass #1
-        let re = pass_1_las(&paths);
-        let bbox = re.0;
-        let totalpts: usize = re.1;
-        info!("count pts={}", totalpts.to_formatted_string(&Locale::en));
-        info!("bbox={:?}", bbox);
-        info!("First pass ✅");
+    //-- pass #1
+    let re = pass_1(&paths, &inputformat);
+    let bbox = re.0;
+    let totalpts: usize = re.1;
+    info!("count pts={}", totalpts.to_formatted_string(&Locale::en));
+    info!("bbox={:?}", bbox);
+    info!("First pass ✅");
 
-        //-- sprinkler
-        let mut rng = thread_rng();
-        let mut sprinkled: HashMap<usize, Point> = HashMap::new();
-        let nc: usize = (totalpts as f64 * 0.001) as usize; //-- TODO: what is a good value?
-        for _i in 0..nc {
-            sprinkled.insert(rng.gen_range(0, totalpts), Point::new());
-        }
-        info!("Sprinkled points: {}", sprinkled.len());
+    //-- sprinkler
+    let mut rng = thread_rng();
+    let mut sprinkled: HashMap<usize, Point> = HashMap::new();
+    let nc: usize = (totalpts as f64 * 0.001) as usize; //-- TODO: what is a good value?
+    for _i in 0..nc {
+        sprinkled.insert(rng.gen_range(0, totalpts), Point::new());
+    }
+    info!("Sprinkled points: {}", sprinkled.len());
 
-        //-- pass #2
-        info!("Second pass ➡️");
-        let mut g: Vec<Vec<usize>> = pass_2_las(&paths, &bbox, cellsize, &mut sprinkled);
-        info!("Second pass ✅");
+    //-- pass #2
+    info!("Second pass ➡️");
+    let mut g: Vec<Vec<usize>> = pass_2(&paths, &bbox, cellsize, &mut sprinkled, &inputformat);
+    info!("Second pass ✅");
 
-        //-- pass #3
-        info!("Thirst pass ➡️");
-        let _re = pass_3_las(&paths, &bbox, cellsize, &mut g, &sprinkled);
-        info!("Third pass ✅");
+    // //-- pass #3
+    info!("Thirst pass ➡️");
+    let _re = pass_3(&paths, &bbox, cellsize, &mut g, &sprinkled, &inputformat);
+    info!("Third pass ✅");
+}
+
+fn pass_1(paths: &Vec<String>, it: &InputType) -> (Vec<f64>, usize) {
+    if *it == InputType::LAS {
+        pass_1_las(&paths)
     } else {
-        //-- inputmode == 1
-        //-- pass #1
-        info!("First pass ➡️");
-        let re = pass_1_xyz(&paths);
-        let bbox = re.0;
-        let totalpts: usize = re.1;
-        info!("count pts={}", totalpts.to_formatted_string(&Locale::en));
-        info!("bbox={:?}", bbox);
-        info!("First pass ✅");
+        pass_1_xyz(&paths)
+    }
+}
 
-        //-- sprinkler
-        let mut rng = thread_rng();
-        let mut sprinkled: HashMap<usize, Point> = HashMap::new();
-        let nc: usize = (totalpts as f64 * 0.001) as usize; //-- TODO: what is a good value?
-        for _i in 0..nc {
-            sprinkled.insert(rng.gen_range(0, totalpts), Point::new());
+fn pass_1_las(paths: &Vec<String>) -> (Vec<f64>, usize) {
+    let mut n: usize = 0;
+    let mut bbox = vec![std::f64::MAX, std::f64::MAX, std::f64::MIN, std::f64::MIN];
+    for path in paths {
+        let reader = las::Reader::from_path(path).expect("Unable to open reader");
+        n += reader.header().number_of_points() as usize;
+        let b = reader.header().bounds();
+        if b.min.x < bbox[0] {
+            bbox[0] = b.min.x;
         }
-        info!("Sprinkled points: {}", sprinkled.len());
+        if b.min.y < bbox[1] {
+            bbox[1] = b.min.y;
+        }
+        if b.max.x < bbox[2] {
+            bbox[2] = b.max.x;
+        }
+        if b.max.y < bbox[3] {
+            bbox[3] = b.max.y;
+        }
+    }
+    (bbox, n)
+}
 
-        //-- pass #2
-        let mut g: Vec<Vec<usize>> = pass_2_xyz(&paths, &bbox, cellsize, &mut sprinkled);
-        info!("Second pass ✅");
+fn pass_1_xyz(paths: &Vec<String>) -> (Vec<f64>, usize) {
+    let mut n: usize = 0;
+    let mut bbox = vec![std::f64::MAX, std::f64::MAX, std::f64::MIN, std::f64::MIN];
+    for path in paths {
+        let f = File::open(path).expect("Unable to open file");
+        let f = BufReader::new(f);
+        for l in f.lines() {
+            let l = l.expect("Unable to read line");
+            let v: Vec<f64> = l.split(' ').map(|s| s.parse().unwrap()).collect();
+            n += 1;
+            //-- minxy
+            for i in 0..2 {
+                if v[i] < bbox[i] {
+                    bbox[i] = v[i];
+                }
+            }
+            //-- maxxy
+            for i in 0..2 {
+                if v[i] > bbox[i + 2] {
+                    bbox[i + 2] = v[i];
+                }
+            }
+        }
+    }
+    (bbox, n)
+}
 
-        //-- pass #3
-        let _re = pass_3_xyz(&paths, &bbox, cellsize, &mut g, &sprinkled);
-        info!("Third pass ✅");
+fn pass_2(
+    paths: &Vec<String>,
+    bbox: &Vec<f64>,
+    cellsize: usize,
+    mut sprinkled: &mut HashMap<usize, Point>,
+    it: &InputType,
+) -> Vec<Vec<usize>> {
+    if *it == InputType::LAS {
+        pass_2_las(&paths, &bbox, cellsize, &mut sprinkled)
+    } else {
+        pass_2_xyz(&paths, &bbox, cellsize, &mut sprinkled)
+    }
+}
+
+fn pass_2_las(
+    paths: &Vec<String>,
+    bbox: &Vec<f64>,
+    cellsize: usize,
+    sprinkled: &mut HashMap<usize, Point>,
+) -> Vec<Vec<usize>> {
+    let width = (bbox[2] - bbox[0]) / cellsize as f64;
+    let height = (bbox[3] - bbox[1]) / cellsize as f64;
+    //-- make it a square to have a quadtree
+    let mut tmp = width;
+    if height > width {
+        tmp = height;
+    }
+    //-- needs to be a power^2 so that is quadtree
+    let cellno: u32 = (2_u32).pow(tmp.log(2.0).ceil() as u32);
+    info!(
+        "Virtual grid is {}x{}; cellsize={}",
+        cellno, cellno, cellsize
+    );
+    let mut g: Vec<Vec<usize>> = vec![vec![0; cellno as usize]; cellno as usize];
+
+    let mut count: usize = 0;
+    for path in paths {
+        let mut reader = las::Reader::from_path(path).expect("Unable to open reader");
+        for each in reader.points() {
+            count += 1;
+            let p = each.unwrap();
+            let gxy: (usize, usize) = get_gx_gy(p.x, p.y, bbox[0], bbox[1], cellsize);
+            g[gxy.0][gxy.1] += 1;
+            //-- chunking
+            if sprinkled.contains_key(&count) == true {
+                let pc = sprinkled.entry(count).or_insert(Point::new());
+                *pc = Point {
+                    x: p.x,
+                    y: p.y,
+                    z: p.z,
+                };
+            }
+        }
+    }
+    g
+}
+
+fn pass_2_xyz(
+    paths: &Vec<String>,
+    bbox: &Vec<f64>,
+    cellsize: usize,
+    sprinkled: &mut HashMap<usize, Point>,
+) -> Vec<Vec<usize>> {
+    let width = (bbox[2] - bbox[0]) / cellsize as f64;
+    let height = (bbox[3] - bbox[1]) / cellsize as f64;
+    //-- make it a square to have a quadtree
+    let mut tmp = width;
+    if height > width {
+        tmp = height;
+    }
+    //-- needs to be a power^2 so that is quadtree
+    let cellno: u32 = (2_u32).pow(tmp.log(2.0).ceil() as u32);
+    info!(
+        "Virtual grid is {}x{}; cellsize={}",
+        cellno, cellno, cellsize
+    );
+    let mut g: Vec<Vec<usize>> = vec![vec![0; cellno as usize]; cellno as usize];
+    let mut count: usize = 0;
+    for path in paths {
+        let f = File::open(path).expect("Unable to open file");
+        let f = BufReader::new(f);
+        for l in f.lines() {
+            let l = l.expect("Unable to read line");
+            count += 1;
+            let v: Vec<f64> = l.split(' ').map(|s| s.parse().unwrap()).collect();
+            let gxy: (usize, usize) = get_gx_gy(v[0], v[1], bbox[0], bbox[1], cellsize);
+            g[gxy.0][gxy.1] += 1;
+            //-- chunking
+            if sprinkled.contains_key(&count) == true {
+                let pc = sprinkled.entry(count).or_insert(Point::new());
+                *pc = Point {
+                    x: v[0],
+                    y: v[1],
+                    z: v[2],
+                };
+            }
+        }
+    }
+    g
+}
+
+fn pass_3(
+    paths: &Vec<String>,
+    bbox: &Vec<f64>,
+    cellsize: usize,
+    mut g: &mut Vec<Vec<usize>>,
+    sprinkled: &HashMap<usize, Point>,
+    it: &InputType,
+) -> io::Result<()> {
+    if *it == InputType::LAS {
+        pass_3_las(&paths, &bbox, cellsize, &mut g, &sprinkled)
+    } else {
+        pass_3_xyz(&paths, &bbox, cellsize, &mut g, &sprinkled)
     }
 }
 
@@ -287,146 +438,9 @@ fn pass_3_xyz(
     Ok(())
 }
 
-fn pass_2_las(
-    paths: &Vec<String>,
-    bbox: &Vec<f64>,
-    cellsize: usize,
-    sprinkled: &mut HashMap<usize, Point>,
-) -> Vec<Vec<usize>> {
-    let width = (bbox[2] - bbox[0]) / cellsize as f64;
-    let height = (bbox[3] - bbox[1]) / cellsize as f64;
-    //-- make it a square to have a quadtree
-    let mut tmp = width;
-    if height > width {
-        tmp = height;
-    }
-    //-- needs to be a power^2 so that is quadtree
-    let cellno: u32 = (2_u32).pow(tmp.log(2.0).ceil() as u32);
-    info!(
-        "Virtual grid is {}x{}; cellsize={}",
-        cellno, cellno, cellsize
-    );
-    let mut g: Vec<Vec<usize>> = vec![vec![0; cellno as usize]; cellno as usize];
-
-    let mut count: usize = 0;
-    for path in paths {
-        let mut reader = las::Reader::from_path(path).expect("Unable to open reader");
-        for each in reader.points() {
-            count += 1;
-            let p = each.unwrap();
-            let gxy: (usize, usize) = get_gx_gy(p.x, p.y, bbox[0], bbox[1], cellsize);
-            g[gxy.0][gxy.1] += 1;
-            //-- chunking
-            if sprinkled.contains_key(&count) == true {
-                let pc = sprinkled.entry(count).or_insert(Point::new());
-                *pc = Point {
-                    x: p.x,
-                    y: p.y,
-                    z: p.z,
-                };
-            }
-        }
-    }
-    g
-}
-
-fn pass_2_xyz(
-    paths: &Vec<String>,
-    bbox: &Vec<f64>,
-    cellsize: usize,
-    sprinkled: &mut HashMap<usize, Point>,
-) -> Vec<Vec<usize>> {
-    let width = (bbox[2] - bbox[0]) / cellsize as f64;
-    let height = (bbox[3] - bbox[1]) / cellsize as f64;
-    //-- make it a square to have a quadtree
-    let mut tmp = width;
-    if height > width {
-        tmp = height;
-    }
-    //-- needs to be a power^2 so that is quadtree
-    let cellno: u32 = (2_u32).pow(tmp.log(2.0).ceil() as u32);
-    info!(
-        "Virtual grid is {}x{}; cellsize={}",
-        cellno, cellno, cellsize
-    );
-    let mut g: Vec<Vec<usize>> = vec![vec![0; cellno as usize]; cellno as usize];
-    let mut count: usize = 0;
-    for path in paths {
-        let f = File::open(path).expect("Unable to open file");
-        let f = BufReader::new(f);
-        for l in f.lines() {
-            let l = l.expect("Unable to read line");
-            count += 1;
-            let v: Vec<f64> = l.split(' ').map(|s| s.parse().unwrap()).collect();
-            let gxy: (usize, usize) = get_gx_gy(v[0], v[1], bbox[0], bbox[1], cellsize);
-            g[gxy.0][gxy.1] += 1;
-            //-- chunking
-            if sprinkled.contains_key(&count) == true {
-                let pc = sprinkled.entry(count).or_insert(Point::new());
-                *pc = Point {
-                    x: v[0],
-                    y: v[1],
-                    z: v[2],
-                };
-            }
-        }
-    }
-    g
-}
-
 fn get_gx_gy(x: f64, y: f64, minx: f64, miny: f64, cellsize: usize) -> (usize, usize) {
     (
         ((x - minx) / cellsize as f64) as usize,
         ((y - miny) / cellsize as f64) as usize,
     )
-}
-
-fn pass_1_las(paths: &Vec<String>) -> (Vec<f64>, usize) {
-    let mut n: usize = 0;
-    let mut bbox = vec![std::f64::MAX, std::f64::MAX, std::f64::MIN, std::f64::MIN];
-    for path in paths {
-        let reader = las::Reader::from_path(path).expect("Unable to open reader");
-        n += reader.header().number_of_points() as usize;
-        let b = reader.header().bounds();
-        if b.min.x < bbox[0] {
-            bbox[0] = b.min.x;
-        }
-        if b.min.y < bbox[1] {
-            bbox[1] = b.min.y;
-        }
-        if b.max.x < bbox[2] {
-            bbox[2] = b.max.x;
-        }
-        if b.max.y < bbox[3] {
-            bbox[3] = b.max.y;
-        }
-    }
-    (bbox, n)
-}
-
-fn pass_1_xyz(paths: &Vec<String>) -> (Vec<f64>, usize) {
-    let mut n: usize = 0;
-    let mut bbox = vec![std::f64::MAX, std::f64::MAX, std::f64::MIN, std::f64::MIN];
-    for path in paths {
-        let f = File::open(path).expect("Unable to open file");
-        let f = BufReader::new(f);
-        for l in f.lines() {
-            let l = l.expect("Unable to read line");
-            let v: Vec<f64> = l.split(' ').map(|s| s.parse().unwrap()).collect();
-            n += 1;
-            //-- minxy
-            for i in 0..2 {
-                if v[i] < bbox[i] {
-                    bbox[i] = v[i];
-                }
-            }
-            //-- maxxy
-            for i in 0..2 {
-                if v[i] > bbox[i + 2] {
-                    bbox[i + 2] = v[i];
-                }
-            }
-        }
-    }
-    (bbox, n)
 }
